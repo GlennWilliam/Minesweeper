e9437441c8b5fdf3fa9baf907cd07d0c76dc812b
glennwillmetafood <glenn.william@metafoodx.com>
2024-09-12 20:37:28 -0700

    Make the game box

--- src/Minesweeper.java
+++ src/Minesweeper.java
@@ -3,6 +3,7 @@ import java.awt.event.*;
 import java.util.ArrayList;
 import java.util.Random;
 import javax.swing.*;
+import org.w3c.dom.Text;
 
 
 public class Minesweeper {
@@ -13,8 +14,17 @@ public class Minesweeper {
     int boardHeight = numRows * tileSize;
 
     JFrame frame = new JFrame("Minesweeper");
+    JLabel textLabel = new JLabel();
+    JPanel textPJlanel = new JPanel();
 
     Minesweeper(){
         frame.setVisible(true);
+        frame.setSize(boardWidth, boardHeight);
+        frame.setLocationRelativeTo(null);
+        frame.setResizable(false);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setLayout(new BorderLayout());
+
+        textLabel.setFont(new Font("Arial", Font.BOLD, 25));
     }
 }
